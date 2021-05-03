b5f1aa30be39fbf528460fa8bd7b479cedfa8f38
Brian aguiar <briantbini@outlook.com>
2021-05-03 10:35:06 -0300

    Escopo criado com classes metodos herenca e sobreposicao de metodo

---
+++ Empresa/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empresa.Entities
+{
+    class OutSourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee()
+        {
+
+        }
+
+        public OutSourcedEmployee(string name, int hours, double valuePerHours, double additionalCharge)
+        : base(name, hours, valuePerHours)
+        {
+
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+
+}
